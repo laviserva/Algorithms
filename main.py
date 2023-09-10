8d70610a4660f7b21d54824eb6521e5372aa675d
laviserva <thundereme@gmail.com>
2023-09-09 21:30:03 -0700

    main script

---
+++ main.py
@@ -0,0 +1,10 @@
+from libs.libs import Insertion_Sort
+
+def main():
+    array = [13, 12, 11, 6, 5]
+    print("array: ", array)
+    out = Insertion_Sort(array)
+    print("Ordenado: ", out)
+
+if __name__ == "__main__":
+    main()
